5a65db98886ff71a97e8b18f0c8ef42deb406ee3
Subudey <subudey_aa@icloud.com>
2026-02-20 20:44:04 +0300

    ConfigManager added

--- src/main/java/subude/gg/ConfigManager.java
+++ src/main/java/subude/gg/ConfigManager.java
@@ -1,4 +1,76 @@
 package subude.gg;
 
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ConfigManager {
+    private final JavaPlugin plugin;
+
+    public int eventInterval;
+    public int eventStartInterval;
+    public int eventDuration;
+    public String stageSound;
+    public int spawnRadius;
+    public int minY;
+    public List<String> startMessage;
+    public List<String> endMessage;
+
+    public final Map<String, CargoType> cargoTypes = new HashMap<>();
+
+    public ConfigManager(JavaPlugin plugin) {
+        this.plugin = plugin;
+        plugin.saveDefaultConfig();
+        loadConfig();
+    }
+
+    private void loadConfig() {
+        eventInterval = plugin.getConfig().getInt("event-interval");
+        eventStartInterval = plugin.getConfig().getInt("event-start-interval");
+        eventDuration = plugin.getConfig().getInt("event-duration");
+        stageSound = plugin.getConfig().getString("stage-sound");
+        spawnRadius = plugin.getConfig().getInt("spawn-radius");
+        minY = plugin.getConfig().getInt("min-y");
+        startMessage = plugin.getConfig().getStringList("start-message");
+        endMessage = plugin.getConfig().getStringList("end-message");
+
+        ConfigurationSection typesSection = plugin.getConfig().getConfigurationSection("cargo-types");
+        if (typesSection != null) {
+
+            for (String key : typesSection.getKeys(false)) {
+                ConfigurationSection typeSection = typesSection.getConfigurationSection(key);
+                CargoType type = new CargoType();
+                type.name = typeSection.getString("name");
+                type.chance = typeSection.getDouble("chance");
+                type.dropTable = new ArrayList<>();
+                List<Map<?, ?>> drops = typeSection.getMapList("drop-table");
+
+                for (Map<?, ?> drop : drops) {
+                    CargoItem item = new CargoItem();
+                    item.material = Material.valueOf(drop.get("material").toString());
+                    item.amount = (int) drop.get("amount");
+                    item.chance = (double) drop.get("chance");
+                    type.dropTable.add(item);
+                }
+                cargoTypes.put(key, type);
+            }
+        }
+    }
+
+    public static class CargoType {
+        public String name;
+        public double chance;
+        public List<CargoItem> dropTable;
+    }
+
+    public static class CargoItem {
+        public Material material;
+        public int amount;
+        public double chance;
+    }
 }
--- src/main/java/subude/gg/SpawnManager.java
+++ src/main/java/subude/gg/SpawnManager.java
@@ -7,11 +7,7 @@ import org.bukkit.entity.minecart.StorageMinecart;
 public class SpawnManager {
     private StorageMinecart minecart;
 
-    public void spawnCart(World world, double x, double y, double z) {
-        Location railLocation = new Location(world,x,y,z); railLocation.getBlock().setType(Material.RAIL);
-        Location cartLocation = railLocation.clone().add(0.5,0,0.5);
-        minecart = world.spawn(cartLocation, StorageMinecart.class);
-    }
+
 
     public StorageMinecart getMinecart() {
         return minecart;
